cdcf1f380dade38763d9ad6326998c5f88eda5da
vlasovad188 <vlasovadasha1808@gmail.com>
2025-10-02 16:26:13 +0300

    Create hash.py

---
+++ hash.py
@@ -0,0 +1,10 @@
+ht = HashTable()
+
+# Добавляем элементы
+ht["apple"] = 5
+ht["banana"] = 3
+ht["orange"] = 8
+
+print("Хеш-таблица:", ht)
+print("apple ->", ht["apple"])
+print("banana ->", ht.get("banana"))
